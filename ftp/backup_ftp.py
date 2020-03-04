d6da3c798b97eb820168e40be609c908123ffff6
ShadowUser17 <linuxforce17@gmail.com>
2020-03-04 21:24:21 +0200

    Modified: .gitignore Modified: ftp/backup_ftp.py Modified: ftp/config.py

--- ftp/backup_ftp.py
+++ ftp/backup_ftp.py
@@ -1,47 +1,42 @@
 #!/usr/bin/env python3
-from sys import stdout
-from sys import stderr
-from traceback import print_exc
+import logging
 from os import path as Path
 from urllib import request as urllib
 #
 #
-from config import backup_dir
-from config import backup_urls
+#from test import (
+from config import (
+    backup_dir,
+    backup_log,
+    backup_urls
+)
 #
 #
 class Downloads:
-    def __init__(self, urls, work):
-        self._work = work if Path.exists(work) else '/tmp'
+    def __init__(self, urls, work, log):
         self._urls = list(map(urllib.urlparse, urls))
+        self._work = work
+        self._log = log
+        self._logging()
+    #
+    def _logging(self):
+        logging.basicConfig(filename=self._log, filemode='w',
+                            format='%(asctime)s %(levelname)s %(message)s',
+                            datefmt='%Y-%m-%d %H:%M:%S',
+                            level=logging.INFO)
     #
     def _download(self, url, file):
         try: urllib.urlretrieve(url, file)
-        except Exception as error: print(error, file=stderr)
-    #
-    def _show(self, prefix, url, file):
-        return '{}: {}:{}{} -> {}'.format(
-            prefix,
-            url.hostname,
-            url.port,
-            url.path,
-            file
-        )
+        except Exception as error: logging.error(error)
     #
     def run(self):
         for item in iter(self._urls):
             url = item.geturl()
             file = Path.basename(item.path)
             file = Path.join(self._work, file)
-            #
-            if not Path.exists(file):
-                print(self._show('Download', item, file))
-                #
-            else:
-                print(self._show('Rewrite', item, file), file=stderr)
-            #
             self._download(url, file)
 #
 #
-if __name__ == '__main__': pass
-
+if __name__ == '__main__':
+    backup = Downloads(backup_urls, backup_dir, backup_log)
+    backup.run()
--- ftp/config.py
+++ ftp/config.py
@@ -1,4 +1,5 @@
-backup_dir='/var/backup/ftp/'
+backup_dir='/var/backup/ftp'
+backup_log='/tmp/ftpbackup.log'
 backup_urls=[
 # Example:
 # ftp://user:password@host:port/file
